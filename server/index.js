96593fbf3eaab48045e5082623388e3203152f5e
juanlubel <juanluis.belda@gmail.com>
2022-12-18 10:10:58 +0100

    adding docker & docker-compose configuration

--- server/index.js
+++ server/index.js
@@ -18,23 +18,24 @@ import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
 import AffiliateStat from "./models/AffiliateStat.js";
 import {
-  dataUser,
-  dataProduct,
-  dataProductStat,
-  dataTransaction,
-  dataOverallStat,
-  dataAffiliateStat,
+    dataUser,
+    dataProduct,
+    dataProductStat,
+    dataTransaction,
+    dataOverallStat,
+    dataAffiliateStat,
 } from "./data/index.js";
+import user from "./models/User.js";
 
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 /* ROUTES */
@@ -43,22 +44,31 @@ app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
+const init = async () => {
+    const users = await User.find({})
+    console.log(users.length)
+    if (users.length === 0) {
+        /* ONLY ADD DATA ONE TIME */
+        AffiliateStat.insertMany(dataAffiliateStat);
+        OverallStat.insertMany(dataOverallStat);
+        Product.insertMany(dataProduct);
+        ProductStat.insertMany(dataProductStat);
+        Transaction.insertMany(dataTransaction);
+        User.insertMany(dataUser);
+    }
+}
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    // .connect("mongodb://root:example@mongo:27017/", {
+        .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+        init()
 
-    /* ONLY ADD DATA ONE TIME */
-    // AffiliateStat.insertMany(dataAffiliateStat);
-    // OverallStat.insertMany(dataOverallStat);
-    // Product.insertMany(dataProduct);
-    // ProductStat.insertMany(dataProductStat);
-    // Transaction.insertMany(dataTransaction);
-    // User.insertMany(dataUser);
-  })
-  .catch((error) => console.log(`${error} did not connect`));
+    })
+    .catch((error) => console.log(`${error} did not connect`));
